aa5314c0db114659f077dfa7ecbef40392d2bbdd
kissrain <cjyang1123@gmail.com>
2017-11-28 23:49:53 +0800

    add TryLock and FairLock eg

---
+++ src/main/java/com/wooyoo/learning/java/concurrency/thread/reentrant/FairLock.java
@@ -0,0 +1,29 @@
+package com.wooyoo.learning.java.concurrency.thread.reentrant;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class FairLock implements Runnable {
+    public static ReentrantLock fairLock = new ReentrantLock(true);
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                fairLock.lock();
+                System.out.println(Thread.currentThread()
+                                         .getName() + "获得锁");
+            }
+            finally {
+                fairLock.unlock();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        FairLock r1 = new FairLock();
+        Thread t1 = new Thread(r1, "Thread_t1");
+        Thread t2 = new Thread(r1, "Thread_t2");
+        t1.start();
+        t2.start();
+    }
+}
