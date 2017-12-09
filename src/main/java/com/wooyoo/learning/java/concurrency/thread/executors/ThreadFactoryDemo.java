9a703de60dcebb09a700d36e399e5c1a6d3d19ed
kissrain <cjyang1123@gmail.com>
2017-12-09 14:49:48 +0800

    add ThreadFactory demo

---
+++ src/main/java/com/wooyoo/learning/java/concurrency/thread/executors/ThreadFactoryDemo.java
@@ -0,0 +1,36 @@
+package com.wooyoo.learning.java.concurrency.thread.executors;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class ThreadFactoryDemo {
+    public static class MyTask implements Runnable {
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":Thread ID:" + Thread.currentThread()
+                                                                                  .getId());
+            try {
+                Thread.sleep(100);
+            }
+            catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        MyTask task = new MyTask();
+        ExecutorService es = new ThreadPoolExecutor(5, 5, 0L, TimeUnit.MILLISECONDS, new SynchronousQueue<>(), r -> {
+            Thread t = new Thread(r);
+            t.setDaemon(true);
+            System.out.println("create " + t);
+            return t;
+        });
+        for (int i = 0; i < 5; i++) {
+            es.submit(task);
+        }
+        Thread.sleep(2000);
+    }
+}
